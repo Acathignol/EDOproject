c0e7bb04a986c4d1841ae1fb91b6b55271c49b77
Annie Cathignol <annie.cathignol@insa-lyon.fr>
2016-01-14 13:39:02 +0100

    exo6

---
+++ exo6_3.m
@@ -0,0 +1,14 @@
+for i =1:1:1000
+    [T,Y]=ode23s(@(t,y)[y(2),(i*(1-y(1)*y(1))*y(2)-y(1))]', [0 6000],[1 1]);
+    MU=i*ones(length(T),1);
+    plot3(T,MU,Y(:,1),'-');
+    if (i==1)
+        hold on 
+    end;
+end;
+hold off
+xlabel('t')
+ylabel('mu')
+zlabel('y')
+
+
