0e06bd089770e51624b460f88bf47f78c7ba3856
Annie Cathignol <annie.cathignol@insa-lyon.fr>
2016-01-14 14:54:03 +0100

    exo 6 figures pas de 1

--- exo6_3.m
+++ exo6_3.m
@@ -1,5 +1,5 @@
-for i =1:1:1000
-    [T,Y]=ode23s(@(t,y)[y(2),(i*(1-y(1)*y(1))*y(2)-y(1))]', [0 6000],[1 1]);
+for i =1:20:1000
+    [T,Y]=ode45(@(t,y)[y(2),(i*(1-y(1)*y(1))*y(2)-y(1))]', [0 6000],[1 1]);
     MU=i*ones(length(T),1);
     plot3(T,MU,Y(:,1));
     if (i==1)
@@ -11,4 +11,15 @@ xlabel('t')
 ylabel('mu')
 zlabel('y')
 
-
+%for i =1:1:1000
+%    [T,Y]=ode23s(@(t,y)[y(2),(i*(1-y(1)*y(1))*y(2)-y(1))]', [0 6000],[1 1]);
+%    MU=i*ones(length(T),1);
+%    plot3(T,MU,Y(:,1));
+%    if (i==1)
+%        hold on 
+%    end;
+%end;
+%hold off
+%xlabel('t')
+%ylabel('mu')
+%zlabel('y')
