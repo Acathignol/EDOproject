fa14bb1257a5638d8e63f46a8005d6a1666bdf2a
Annie Cathignol <annie.cathignol@insa-lyon.fr>
2016-01-14 19:30:38 +0100

    axo6 changes

--- exo6_3.m
+++ exo6_3.m
@@ -1,20 +1,8 @@
-for i =1:20:1000
-    [T,Y]=ode45(@(t,y)[y(2),(i*(1-y(1)*y(1))*y(2)-y(1))]', [0 6000],[1 1]);
-    MU=i*ones(length(T),1);
-    plot3(T,MU,Y(:,1));
-    if (i==1)
-        hold on 
-    end;
-end;
-hold off
-xlabel('t')
-ylabel('mu')
-zlabel('y')
-
-%for i =1:1:1000
-%    [T,Y]=ode23s(@(t,y)[y(2),(i*(1-y(1)*y(1))*y(2)-y(1))]', [0 6000],[1 1]);
+%for i =1:20:1000
+%    [T,Y]=ode45(@(t,y)[y(2),(i*(1-y(1)*y(1))*y(2)-y(1))]', [0 6000],[1 1]);
 %    MU=i*ones(length(T),1);
-%    plot3(T,MU,Y(:,1));
+    %plot3(T,MU,Y(:,1));
+
 %    if (i==1)
 %        hold on 
 %    end;
@@ -23,3 +11,18 @@ zlabel('y')
 %xlabel('t')
 %ylabel('mu')
 %zlabel('y')
+
+for i =1:200:1000
+    [T,Y]=ode23s(@(t,y)[y(2),(i*(1-y(1)*y(1))*y(2)-y(1))]', [0 6000],[1 1]);
+    MU=i*ones(length(T),1);
+    plot3(T,MU,Y(:,1));
+    %plot(Y(:,1),Y(:,2));
+    if (i==1)
+        hold on 
+    end;
+end;
+hold off
+%xlabel('t')
+%ylabel('mu')
+%zlabel('y')
+title('Portrait de phase des solutions y')
