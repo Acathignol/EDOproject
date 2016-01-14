b32654574c36e9cf5c97f904469c7fc008bbb067
Louis Becquey <louis.becquey@insa-lyon.fr>
2016-01-14 15:07:35 +0100

    images

--- ex4.py
+++ ex4.py
@@ -1,7 +1,17 @@
 import  matplotlib.pyplot as plt
 from numpy import *
 
-temps=range(0,100)
 
 def holling2(N):
-	
+	return 0.6*N/float(1+0.6*h*N)	
+
+
+n=range(1500)
+h=0.02
+fn=map(holling2,n)
+plt.plot(n,fn,'g')
+
+plt.xlabel('N')
+plt.ylabel('f(N)=0.6N/(1+0.6hN)')
+plt.title('Proies mangees/predateur/temps = f( proies )')
+plt.show()
